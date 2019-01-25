0552b12f766ecd22a6eb7c6ec734c35f99c4d12b
logicoffee <riemann1618@gmail.com>
2019-01-25 10:34:32 +0900

    Improve test suite

--- test/active_excel_test.rb
+++ test/active_excel_test.rb
@@ -15,7 +15,10 @@ class ActiveExcelTest < Minitest::Test
     file_path = File.join(@base_path, "invalid_header.xlsx")
     wb = RubyXL::Parser.parse(file_path)
     refute_nil wb["users"]
+
     validated_records = User.build_from_excel(file_path)
+    assert_equal 2, validated_records.all_records.size
+    assert_equal 0, validated_records.valid_records.size
     assert_equal 2, validated_records.invalid_records.size
   end
 
